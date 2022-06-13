2939e7652b0642926bc4aa0d9217bfab2ab74eb1
sosow0212 <sosow0212@naver.com>
2022-06-13 22:33:50 +0900

    feat: week 2 과제 완료

--- src/hello.java
+++ src/hello.java
@@ -2,5 +2,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("week 1 과제 완료");
+        System.out.println("week 2 과제 완료");
     }
 }
